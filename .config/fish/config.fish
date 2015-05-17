bbb3f9fa54cda1e84069d339e06fe373fd295fd3
Dylan Arbour <dylan.arbour@gmail.com>
2015-05-17 17:25:06 -0400

    Add $SHELL env var for docker-machine

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,19 @@
 # Set default user
 set default_user dylan
 
-# nvm wrapper
+# Load nvm with nvm-wrapper
 source ~/.config/fish/nvm-wrapper/nvm.fish
 
-# rbenv
+# Set path for rbenv
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
 rbenv rehash >/dev/null ^&1
 
-# go
-set -x GOPATH=$HOME/dev/go
+# Set GOPATH for go
+set -x GOPATH $HOME/dev/go
+
+# Set $SHELL env var for docker-machine
+set -x SHELL fish
 
 # Empty the greeting string
 set fish_greeting ""
