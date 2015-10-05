db33bee1537eb6a91d741a7f4b4137d57c01fee7
Dylan Arbour <dylan.arbour@gmail.com>
2015-10-05 14:15:03 -0400

    Use omf over nvm-wrapper for loading nvm

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,8 +1,8 @@
 # Set default user
 set default_user dylan
 
-# Load nvm with nvm-wrapper
-source ~/.config/fish/nvm-wrapper/nvm.fish
+# Load nvm
+# fenv source ~/.nvm/nvm.sh
 
 # Set path for rbenv
 set PATH $HOME/.rbenv/bin $PATH
@@ -18,11 +18,3 @@ set -x SHELL fish
 # Empty the greeting string
 set fish_greeting ""
 
-# Path to Oh My Fish Install
-set -gx OMF_PATH /Users/dylan/.local/share/omf
-
-# Customize Oh My Fish configuration path.
-# #set -gx OMF_CONFIG /Users/dylan/.config/omf
-#
-# # Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
