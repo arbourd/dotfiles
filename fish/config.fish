c5bbee7ed003fead0f9d33cf45cd0ec3e3ca7d47
Dylan Arbour <dylan.arbour@gmail.com>
2016-09-09 15:58:54 -0400

    Add $GOPATH/bin to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,9 @@ eval (python -m virtualfish auto_activation)
 
 # Set GOPATH for go
 set -x GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
 
-# Set $SHELL env var for docker-machine
+# Set $SHELL env var for fish
 set -x SHELL fish
 
 # Empty the greeting string
