766e1153d54b9571a7a8c422cb646b290c726a3f
Dylan Arbour <dylan.arbour@gmail.com>
2016-12-09 16:48:05 -0500

    Remove virtualfish startup call

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,6 @@
 # Set default user
 set default_user dylan
 
-# Virtualfish with auto activation
-eval (python -m virtualfish auto_activation)
-
 # Set GOPATH for go
 set -x GOPATH $HOME/go
 set -x PATH $GOPATH/bin $PATH
