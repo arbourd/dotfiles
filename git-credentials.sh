2445f51d9481042327948e4a7ebd7a8fefda9514
Dylan Arbour <dylan.arbour@gmail.com>
2017-10-07 19:04:42 -0400

    Use osxkeychain over store

--- git-credentials.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Github personal access token: "
-read token
-echo "https://$token:x-oauth-basic@github.com" >> ~/.git-credentials
-echo "Credentials stored in ~/.git-credentials"
