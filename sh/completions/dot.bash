97b3669e85faf982869a3612929881223b3a2101
Dylan Arbour <arbourd@users.noreply.github.com>
2026-05-12 14:53:49 -0400

    Add completions for version command

--- sh/completions/dot.bash
+++ sh/completions/dot.bash
@@ -1,7 +1,7 @@
 _dot() {
     local cur="${COMP_WORDS[COMP_CWORD]}"
     [[ $COMP_CWORD -eq 1 ]] || return
-    COMPREPLY=($(compgen -W "help init update link install install-defaults install-brew install-fisher install-vim" -- "$cur"))
+    COMPREPLY=($(compgen -W "help init link install install-defaults install-brew install-fisher install-vim update version" -- "$cur"))
 }
 
 complete -F _dot dot
